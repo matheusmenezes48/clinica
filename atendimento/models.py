438670c8829674d8a9e56c939ab6b7e93b83a991
Aluno <you@example.com>
2018-11-22 21:03:18 -0300

    funcionalidade de listagem

--- atendimento/models.py
+++ atendimento/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50),
+    dt_nasc = models.DateField(),
+    cpf = models.CharField(max_length=11),
+    sexo = models.CharField(max_length=1)
 
--- atendimento/views.py
+++ atendimento/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from . models import Cliente
 
 def home(request):
     return render(request, 'home.html',{})
+
+
+def cliente_list(request):
+    clientes = Cliente.objects.all()
+    return render(request,'cliente/list.html',{'clientes':clientes})
+
+
