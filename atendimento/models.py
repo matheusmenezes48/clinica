c12057b6bbddeb37caad3cf78832366fba0717aa
Aluno <you@example.com>
2018-11-23 21:11:07 -0300

    nada a declarar

--- atendimento/models.py
+++ atendimento/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Cliente(models.Model):
-    nome = models.CharField(max_length=50),
-    dt_nasc = models.DateField(),
-    cpf = models.CharField(max_length=11),
+    nome = models.CharField(max_length=50)
+    dt_nasc = models.DateField()
+    cpf = models.CharField(max_length=11)
     sexo = models.CharField(max_length=1)
 
